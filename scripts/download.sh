ebc1655361e44109579263ce0317661c41d0d1c5
crapthings <crapthings@gmail.com>
2024-02-29 11:14:57 +0800

    update

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,1 +1,1 @@
-curl --output model.safetensors -JLOf --max-time 0 https://civitai.com/api/download/models/90072
+curl --output ./ESRGAN/4x-UltraSharp.pth -JLOf --max-time 0 https://huggingface.co/crapthings/4x-UltraSharp/resolve/main/4x-UltraSharp.pth
